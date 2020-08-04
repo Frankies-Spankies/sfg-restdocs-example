759baf4eb969b6a9856dcadcb57c10656f855aae
FrankiPanki <puchicher@gmail.com>
2020-08-04 11:37:53 -0500

    final de la seccion del curso microservicion con spring usando RestDocs

--- src/test/java/guru/springframework/sfgrestdocsexample/web/controller/BeerControllerTest.java
+++ src/test/java/guru/springframework/sfgrestdocsexample/web/controller/BeerControllerTest.java
@@ -6,13 +6,22 @@ import guru.springframework.sfgrestdocsexample.repositories.BeerRepository;
 import guru.springframework.sfgrestdocsexample.web.model.BeerDto;
 import guru.springframework.sfgrestdocsexample.web.model.BeerStyleEnum;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.constraints.ConstraintDescriptions;
+import org.springframework.restdocs.payload.FieldDescriptor;
+import org.springframework.restdocs.payload.PayloadDocumentation;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.util.StringUtils;
+
 
 import java.math.BigDecimal;
 import java.util.Optional;
@@ -20,9 +29,24 @@ import java.util.UUID;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+
+//****** Hay que cambiar el import del Builders del MockMvc al de rest docs para que funcione con el "Documentador"********
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.request.RequestDocumentation.*;
+import static org.springframework.restdocs.snippet.Attributes.key;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+
+//Configura MockMVC para que funcione con RestDocumentation
+//Esta forma de configurar permite que no se levante todo el contexto de spring, dejndo lo que provee @WebMvcTest
+//Mas referencia https://docs.spring.io/spring-restdocs/docs/2.0.4.RELEASE/reference/html5/
+@ExtendWith(RestDocumentationExtension.class)
+@AutoConfigureRestDocs(uriScheme = "http", uriHost = "dev.frank.cursos", uriPort = 80)
+//Solo carga el controlador con el runner de Mockito por eso ocupa el @ComponentScan
+//Mas referencia https://stackoverflow.com/questions/39865596/difference-between-using-mockmvc-with-springboottest-and-using-webmvctest
 @WebMvcTest(BeerController.class)
 @ComponentScan(basePackages = "guru.springframework.sfgrestdocsexample.web.mappers")
 class BeerControllerTest {
@@ -40,8 +64,33 @@ class BeerControllerTest {
     void getBeerById() throws Exception {
         given(beerRepository.findById(any())).willReturn(Optional.of(Beer.builder().build()));
 
-        mockMvc.perform(get("/api/v1/beer/" + UUID.randomUUID().toString()).accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk());
+        //Al ser un solo parametro se pasa en el path, los parametros del path se tienen que poner explicitos
+        ResultActions perform = mockMvc.perform(get("/api/v1/beer/{beerId}", UUID.randomUUID().toString()) //Path parameters
+                .param("is cold", "yes")//query parameters en post son campos de formulario
+                .accept(MediaType.APPLICATION_JSON));
+
+                perform.andExpect(status().isOk());
+
+                //El nombre del documento No debe empezar con diagonal p.e /v1/beer
+                //Al agregar -get la documentacion de este endpoint se separa de el endpoint v1/beer-new
+                perform.andDo(document("v1/beer-get",
+                        pathParameters(
+                                parameterWithName("beerId").description("UUID de la cerveza deseada")),
+                        requestParameters(
+                                parameterWithName("is cold").description("Is Beer Cold Query parameter")),
+                        responseFields(
+                                fieldWithPath("id").description("Id of de Beer"),
+                                fieldWithPath("version").description("Version of the beer"),
+                                fieldWithPath("createdDate").description("Creation date"),
+                                fieldWithPath("lastModifiedDate").description("Last modified date"),
+                                fieldWithPath("beerName").description("Name of the beer"),
+                                fieldWithPath("beerStyle").description("Style of the beer"),
+                                fieldWithPath("upc").description("UPC code of the beer"),
+                                fieldWithPath("price").description("Price of the beer"),
+                                fieldWithPath("quantityOnHand").description("Quantity on Hand")
+                        )
+                    )
+                 );
     }
 
     @Test
@@ -49,10 +98,26 @@ class BeerControllerTest {
         BeerDto beerDto =  getValidBeerDto();
         String beerDtoJson = objectMapper.writeValueAsString(beerDto);
 
-        mockMvc.perform(post("/api/v1/beer/")
+        ConstrainedFields fields = new ConstrainedFields(BeerDto.class);
+
+        ResultActions perform = mockMvc.perform(post("/api/v1/beer/")
                 .contentType(MediaType.APPLICATION_JSON)
-                .content(beerDtoJson))
-                .andExpect(status().isCreated());
+                .content(beerDtoJson));
+
+                perform.andExpect(status().isCreated());
+
+                perform.andDo(document("v1/beer-new",
+                        requestFields(
+                                fields.withPath("id").ignored(),
+                                fields.withPath("version").ignored(),
+                                fields.withPath("createdDate").ignored(),
+                                fields.withPath("lastModifiedDate").ignored(),
+                                fields.withPath("beerName").description("Name of the beer"),
+                                fields.withPath("beerStyle").description("Style of the beer"),
+                                fields.withPath("upc").description("UPC code of the beer"),
+                                fields.withPath("price").description("Price of the beer"),
+                                fields.withPath("quantityOnHand").ignored()
+                        )));
     }
 
     @Test
@@ -74,6 +139,21 @@ class BeerControllerTest {
                 .upc(123123123123L)
                 .build();
 
+    }
+
+    private static class ConstrainedFields {
+
+        private final ConstraintDescriptions constraintDescriptions;
+
+        ConstrainedFields(Class<?> input) {
+            this.constraintDescriptions = new ConstraintDescriptions(input);
+        }
+
+        private FieldDescriptor withPath(String path) {
+            return fieldWithPath(path).attributes(key("constraints").value(StringUtils
+                    .collectionToDelimitedString(this.constraintDescriptions
+                            .descriptionsForProperty(path), ". ")));
+        }
     }
 
 }
